45ba9a6121ccc66303c917a33fc03362b46eec5e
gabebaggabe <gabejoseph09@gmail.com>
2026-07-15 19:35:50 -0400

    first commit

---
+++ lua/config/keybinds.lua
@@ -0,0 +1,6 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+
+vim.keymap.set({"n", "v"}, "<leader>y", '"+y', { desc = "Yank to system clipboard" })
+vim.keymap.set({"n", "v"}, "<leader>p", '"+p', { desc = "Paste from system clipboard" })
+vim.keymap.set({"n", "v"}, "<leader>d", '"+d', { desc = "Delete to system clipboard" })
