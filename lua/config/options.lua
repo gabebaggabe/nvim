3ce6f782f57914c1a965fcea5ada0038470aabb3
gabebaggabe <149747119+gabebaggabe@users.noreply.github.com>
2026-07-20 02:21:58 +0000

    Refactor Vim options into a table structure

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,32 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.cursorline = true
-vim.opt.shiftwidth = 4
-vim.opt.clipboard = "unnamedplus"
+local options = {
+
+	-- random shit
+	shiftwidth = 4,
+	ttyfast = true,
+	smoothscroll = true,
+
+	-- numbers
+	number = true,
+	relative = true,
+
+	-- indenting stuff
+	smarttab = true,
+	cindent = true,
+	autoindent = true,
+	tabstop = 4,
+
+	-- cursor line
+	cursorline = true,
+
+	-- clipboard
+	clipboard = "unnamedplus",
+
+}
+
+for k, v in pairs(options) do
+	vim.opt[k] = v
+end
+
+vim.diagnostic.config({
+	signs = false,
+})
